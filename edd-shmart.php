2e4140d55a84f0235e835a34ecbfed9cbf77232d
Ritesh Patel <ritzpatel91@gmail.com>
2016-01-13 16:59:41 +0530

    remove EDD license lib and related code

--- edd-shmart.php
+++ edd-shmart.php
@@ -59,14 +59,6 @@ if ( !defined( 'SPG_DIRNAME' ) ) {
     define( 'SPG_DIRNAME', dirname( plugin_basename(__FILE__) ) );
 }
 
-// Load the EDD license handler only if not already loaded. Must be placed in the main plugin file
-if( ! class_exists( 'EDD_License' ) ) {
-	include( SPG_ROOT . 'lib/edd-license/EDD_License_Handler.php' );
-}
-
-// Update this details on version update otherwise update won't work.
-$license = new EDD_License( __FILE__, 'Shmart Payment Gateway For EDD', '1.0', 'rtCamp', null, 'https://rtcamp.com' );
-
 /**
  * Shmart_Payment_Gateway
  *
